b546d7a10b95001cb7bd1273bbaa55172de77e98
David Rodríguez <deivid.rodriguez@riseup.net>
2020-05-21 20:15:21 +0200

    Use `ruby2_keywords` for the moment (#205)
    Without this gem, current activeadmin version becomes incompatible with
    the master branch of `arbre`. It shouldn't be an issue as long as we
    update activeadmin's code to use keyword arguments everywhere, set a
    minimum dependency on the next `arbre` and release both gems at the same
    time, but I think it's easier to be conservative.

--- arbre.gemspec
+++ arbre.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |s|
   s.required_ruby_version = '>= 2.5'
 
   s.add_dependency("activesupport", ">= 3.0.0", "< 6.1")
+  s.add_dependency("ruby2_keywords", ">= 0.0.2")
 end
--- lib/arbre/context.rb
+++ lib/arbre/context.rb
@@ -1,4 +1,5 @@
 require 'arbre/element'
+require 'ruby2_keywords'
 
 module Arbre
 
@@ -74,21 +75,11 @@ module Arbre
     # Webservers treat Arbre::Context as a string. We override
     # method_missing to delegate to the string representation
     # of the html.
-    if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7.a")
-      def method_missing(method, *args, **kwargs, &block)
-        if cached_html.respond_to? method
-          cached_html.send method, *args, **kwargs, &block
-        else
-          super
-        end
-      end
-    else
-      def method_missing(method, *args, &block)
-        if cached_html.respond_to? method
-          cached_html.send method, *args, &block
-        else
-          super
-        end
+    ruby2_keywords def method_missing(method, *args, &block)
+      if cached_html.respond_to? method
+        cached_html.send method, *args, &block
+      else
+        super
       end
     end
 
