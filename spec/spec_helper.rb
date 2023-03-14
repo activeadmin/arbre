1b68979219bc3bd99b7121ee2fe30fd078d09f17
Javier Julio <jjfutbol@gmail.com>
2023-03-14 11:53:56 -0400

    Add simplecov setup to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
-require 'support/bundle'
 
+if ENV.fetch('COVERAGE', false)
+  require 'simplecov'
+  require 'simplecov-cobertura'
+  SimpleCov.start do
+    add_filter %r{^/spec/}
+    minimum_coverage 90
+    maximum_coverage_drop 0.2
+    formatter SimpleCov::Formatter::CoberturaFormatter
+  end
+end
+
+require 'support/bundle'
 require 'arbre'
 
 def arbre(&block)
