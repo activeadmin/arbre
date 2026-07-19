a16f6619bb440a773332a27d2f19be38689bcdea
Geremia Taglialatela <tagliala.dev@gmail.com>
2026-07-19 12:24:42 +0200

    Update SimpleCov to 1.0
    Remove the explicit filter on `/spec`, which is now ignored by default.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ if ENV.fetch('COVERAGE', false)
   require 'simplecov'
   require 'simplecov-cobertura'
   SimpleCov.start do
-    add_filter %r{^/spec/}
     minimum_coverage 90
     maximum_coverage_drop 0.2
     formatter SimpleCov::Formatter::CoberturaFormatter
